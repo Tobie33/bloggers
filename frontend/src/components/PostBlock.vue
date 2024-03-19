2d709def132492fd7562525d6cdb0bdd87a39c45
tobie33 <tobie6533@gmail.com>
2024-03-19 22:34:42 +0800

    frontend stuff

---
+++ frontend/src/components/PostBlock.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="post-block">
+    <div id="post-title">
+      <h3>{{ post.title }}</h3>
+    </div>
+    <div id="post-content">
+      <p>{{ post.content }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostBlock',
+  props:['post'],
+  setup(){
+
+  }
+}
+</script>
+
+<style>
+.post-block{
+
+  min-height:175px;
+  width:450px;
+}
+
+#post-title{
+  background-color: #B2B5E0;
+  border-radius:10px 10px 0 0;
+  padding: 10px 0;
+}
+p{
+  margin-top:0;
+}
+#post-content{
+  border-radius:0 0 10px 10px ;
+  min-height:150px;
+  background-color: #F8F6F0;
+  padding:15px;
+}
+
+h3{
+  margin:0;
+}
+
+</style>
---
+++ frontend/src/views/Posts.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Posts Page</h1>
+  <PostList :posts="posts"/>
+</template>
+
+<script>
+
+import {getPosts} from '@/hooks/PostHooks'
+import PostList from '@/components/PostList.vue'
+
+
+export default {
+  name: 'Posts',
+  components: {PostList},
+  setup(){
+
+    const {posts, error} = getPosts()
+
+    return {posts, error}
+  }
+
+}
+</script>
+
+<style>
+
+</style>
